bfc26a75188460c15d47a7b18926ddae12a07a2b
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-13 10:52:32 +0100

    * test/pp-test-main.el: Tests for additional project settings.

--- project-persist.el
+++ project-persist.el
@@ -40,6 +40,17 @@
 (defvar project-persist-current-project-root-dir nil
   "The root directory of the project currently loaded by project-persist.")
 
+(defvar project-persist-additional-settings '()
+  "A list of additional keys to store in the project settings file (the defaults are 'name and 'root-dir).
+The format should be a cons cell ('key . read-function); e.g. ('name . (lambda () (read-from-buffer \"Project name: \"))).")
+
+
+;; Internal variables
+(defvar pp/settings-file-name "pp-settings.txt"
+  "Name of the default settings file to write in each project's settings directory.")
+
+(defvar pp/settings-hash (pp/reset-hashtable)
+  "Settings hashtable to be written to the project settings file.")
 
 ;; Interactive functions
 (defun project-persist-create (root-dir name)
@@ -53,13 +64,9 @@
        (list i-root-dir i-name))))
     (condition-case err
         (progn
-          (pp/project-setup name root-dir)
+          (pp/project-setup root-dir name)
           (pp/project-open name))
-      (error (progn
-               (ding)
-               (message "%s" (error-message-string err))
-               (sit-for 1)
-               (project-persist-create)))))
+      (error (pp/signal-error err))))
 
 (defun project-persist-save ())
 
@@ -68,36 +75,89 @@
 (defun project-persist-list ())
 
 ;; Internal functions
+(defun pp/reset-hashtable ()
+  "Empty the hashtable containing project settings."
+  (setq pp/settings-hash (make-hash-table :test 'equal)))
+
+(defun pp/signal-error (err &optional func)
+  "Ding and message the error string, optionally continuing with a given function."
+  (ding)
+  (message "%s" (error-message-string err))
+  (sit-for 1)
+  (when func (funcall func)))
+
 (defun pp/project-exists (name) nil)
 
-(defun pp/project-setup (name root-dir)
-  "Set up a project with name name and root directory dir."
+(defun pp/project-setup (root-dir name)
+  "Set up a project with name name and root directory root-dir."
   (if (string= name "") (error "Project name is empty"))
   (if (pp/project-exists name) (error "Project %s already exists." name))
   (run-hooks 'project-persist-before-create-hook)
   (let ((settings-dir (pp/settings-dir-from-name name)))
     (pp/make-settings-dir settings-dir)
-    (pp/project-write name root-dir settings-dir)
+    (pp/reset-hashtable)
+    (pp/settings-set 'root-dir root-dir)
+    (pp/settings-set 'name name)
+    (pp/set-additional-settings)
+    (pp/project-write settings-dir)
     (run-hooks 'project-persist-after-create-hook)))
 
-(defun pp/project-write (name root-dir settings-dir)
-  (let ((settings `((project-name . ,name)(project-root-dir . ,root-dir)))
-        (settings-file (expand-file-name "pp-settings.txt" settings-dir)))
-    (with-temp-buffer
-      (print settings (current-buffer))
-      (pp/write-to-settings settings-file (buffer-string)))))
+(defun pp/set-additional-settings ()
+  (let ((settings-keys project-persist-additional-settings))
+    (message "setting additional settings: %S" settings-keys)
+    (while settings-keys
+      (let ((setting (car settings-keys)))
+        (message "setting: %S" setting)
+        (let ((setting-key (car setting))(setting-value (funcall (cdr setting))))
+          (message "key: %S value: %S" setting-key setting-value)
+          (pp/settings-set setting-key setting-value)
+          (setq settings-keys (cdr settings-keys)))))))
 
-(defun pp/write-to-settings (settings-file settings-string)
-  (with-temp-file settings-file (insert settings-string)))
-
-(defun pp/settings-dir-from-name (name)
-  (concat (expand-file-name name project-persist-settings-dir)))
+(defun pp/settings-set (key value)
+  "Set project setting key to value."
+  (puthash key value pp/settings-hash))
 
 (defun pp/project-open (name)
   "Open the project named name."
-  (message (format "Opening project %s" name)))
+  (let ((settings-file
+         (expand-file-name
+          pp/settings-file-name (pp/settings-dir-from-name name))))
+    (let ((settings (pp/project-read-settings-from-string
+                     (pp/get-settings-file-contents settings-file))))
+      (pp/apply-project-settings settings))))
+
+(defun pp/apply-project-settings (settings)
+  "Make the settings read from the project settings file current."
+  ;; set hashtable then set global vars
+  )
+
+(defun pp/get-settings-file-contents (settings-file)
+  "Read and return contents of settings-file"
+  (with-temp-buffer
+    (insert-file-contents settings-file)
+    (buffer-string)))
+
+(defun pp/project-read-settings-from-string (settings-string)
+  "Read and return the project settings hash from the given file."
+  (read-from-string settings-string))
+
+(defun pp/project-write (settings-dir)
+  "Write project settings to the given settings directory."
+  (let ((settings-file (expand-file-name pp/settings-file-name settings-dir)))
+    (with-temp-buffer
+      (print pp/settings-hash (current-buffer))
+      (pp/write-to-settings settings-file (buffer-string)))))
+
+(defun pp/write-to-settings (settings-file settings-string)
+  "Write the given string representing project settings to the given file."
+  (with-temp-file settings-file (insert settings-string)))
+
+(defun pp/settings-dir-from-name (name)
+  "Return the settings directory for the project based on its name."
+  (concat (expand-file-name name project-persist-settings-dir)))
 
 (defun pp/make-settings-dir (settings-dir)
+  "Create the project settings directory if it doesn't already exist, creating the project-persist root settings directory if necessary."
   (unless (file-exists-p project-persist-settings-dir)
     (make-directory project-persist-settings-dir))
   (unless (file-exists-p settings-dir)
--- test/pp-test-main.el
+++ test/pp-test-main.el
@@ -9,12 +9,12 @@
 (ert-deftest pp-test/empty-project-name-signals-error ()
   "Test that attempting to create a project with an empty name signals an error."
   (flet ((pp/make-settings-dir (sd) t)(pp/project-write (n rd sd) t))
-    (should-error (project-persist-create "/test" ""))))
+    (should-error (pp/project-setup "/test" ""))))
 
 (ert-deftest pp-test/existing-project-name-signals-error ()
   "Test that attempting to create a project with an existing name signals an error."
   (flet ((pp/project-exists (name) t)(pp/make-settings-dir (sd) t)(pp/project-write (n rd sd) t))
-    (should-error (project-persist-create "/test" "test"))))
+    (should-error (pp/project-setup "/test" "test"))))
 
 (ert-deftest pp-test/correct-settings-dir-from-name ()
   "Test that the correct directory name for the settings file is returned."
@@ -29,17 +29,46 @@
        (project-root-dir "/test/project-root-dir"))
     (flet ((pp/write-to-settings (settings-file settings-string)
                (should (equal settings-file "/test/settings-dir/pp-settings.txt"))))
-      (pp/project-write project-name project-root-dir settings-dir))))
+      (pp/project-write settings-dir))))
 
 (ert-deftest pp-test/settings-written-correctly ()
-  "Test that project settings are written correctly."
+  "Test that project settings are stored correctly."
+  (setq project-persist-additional-settings nil)
+  (pp/reset-hashtable)
   (let
-      ((settings-dir "/test/settings-dir")
-       (project-name "test-project-name")
+      ((project-name "test-project-name")
        (project-root-dir "/test/project-root-dir")
-       (settings-text "\n((project-name . \"test-project-name\") (project-root-dir . \"/test/project-root-dir\"))\n"))
+       (settings-text "\n#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8 data (root-dir \"/test/project-root-dir\" name \"test-project-name\"))\n"))
     (flet ((pp/write-to-settings (settings-file settings-string)
                                  (with-temp-buffer
                                    (insert settings-string)
                                    (should (equal (buffer-string) settings-text)))))
-      (pp/project-write project-name project-root-dir settings-dir))))
+      (pp/settings-set 'root-dir project-root-dir)
+      (pp/settings-set 'name project-name)
+      (pp/project-write "/test-settings-dir"))))
+
+(ert-deftest pp-test/additional-settings-written-correctly ()
+  "Test that additional project settings are stored correctly."
+  (setq project-persist-additional-settings nil)
+  (pp/reset-hashtable)
+  (let
+      ((project-name "test-project-name")
+       (project-root-dir "/test/project-root-dir")
+       (settings-text "\n#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8 data (root-dir \"/test/project-root-dir\" name \"test-project-name\" test \"test setting\"))\n"))
+    (flet ((pp/write-to-settings (settings-file settings-string)
+                                 (with-temp-buffer
+                                   (insert settings-string)
+                                   (should (equal (buffer-string) settings-text)))))
+      (pp/settings-set 'root-dir project-root-dir)
+      (pp/settings-set 'name project-name)
+      (add-to-list 'project-persist-additional-settings '(test . (lambda () (concat "test setting"))))
+      (pp/set-additional-settings)
+      (pp/project-write "/test-settings-dir"))))
+
+(ert-deftest pp-test/settings-read-correctly ()
+  "Test that project settings are read back in correctly."
+  )
+
+(ert-deftest pp-test/settings-applied-correctly ()
+  "Test that project settings are applied correctly."
+  )
