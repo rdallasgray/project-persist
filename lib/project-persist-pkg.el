59a2b0a673385d1c0166a7d870f783b8e377e7d5
Robert Dallas Gray <robert@geo.me>
2018-09-06 13:59:44 +0100

    Fixup package.el

--- lib/project-persist-pkg.el
+++ lib/project-persist-pkg.el
@@ -1,1 +1,1 @@
-(define-package "project-persist" "0.3.2" "A minor mode to allow loading and saving of project settings." 'nil)
+(define-package "project-persist" "1.0.0" "A minor mode to allow loading and saving of project settings." 'nil)
